7743087b8e89361bf898a8de81014e39be523a90
Tristan BASLER <tristan.basler@gmail.com>
2022-06-06 13:58:43 -0400

    Add new description

--- TSF_Project/Forecast_vizualisation_specific.py
+++ TSF_Project/Forecast_vizualisation_specific.py
@@ -75,7 +75,25 @@ class Application:
         explanation_princip = [
             Title("Method Explanation"),
             Paragraph(
-                "The goal of this app is to study the result ot several method of time series Forecasting on different databases."
+                """
+                The goal of this app is to study the result of several methods of time series Forecasting on different databases."
+                
+                We can define two major interests in this study.
+                
+                1. A Supply Chain Problem :
+                Knowing the next purchase of a product and his quantity is needed for the optimization of the supply chain.
+                Indeed, knowing this information, you can choose how and with which product you want to a fill a truck.
+                This is the major asset for Amazon. 
+                
+                2. A Commercial Problem :
+                The second information you can extract from this study is the future behavior.
+                If you see a reduction of the purchase of one customer, this can mean this customer is going to leave 
+                your company for another.
+                So, you need to act in order to avoid that
+                
+                In a more complex way, the forecast is a way of the future can append. So, if the reality is very different
+                from the forecast, something must have arrived.
+                """
             ),
         ]
         explanation_model = [
@@ -193,8 +211,8 @@ class Application:
             test_index.freq = None
             test_index = pd.DatetimeIndex(test_index)
             diff = (
-                abs(y_pred.loc[test_index] - y_true.loc[test_index])
-                / y_true.loc[test_index]
+                    abs(y_pred.loc[test_index] - y_true.loc[test_index])
+                    / y_true.loc[test_index]
             )
             fig_error = px.bar(diff)
             fig_error.update_layout(
@@ -225,13 +243,13 @@ class Application:
 
     def specific_graph(self):
         if not all(
-            (
-                self.model(),
-                self.database(),
-                self.date(),
-                self.input_number(),
-                self.col_option(),
-            )
+                (
+                        self.model(),
+                        self.database(),
+                        self.date(),
+                        self.input_number(),
+                        self.col_option(),
+                )
         ):
             return
         else:
@@ -267,8 +285,8 @@ class Application:
             test_index.freq = None
             test_index = pd.DatetimeIndex(test_index)
             diff = (
-                abs(y_pred.loc[test_index] - y_true.loc[test_index])
-                / y_true.loc[test_index]
+                    abs(y_pred.loc[test_index] - y_true.loc[test_index])
+                    / y_true.loc[test_index]
             )
             fig_error = px.bar(diff)
             fig_error.update_layout(
